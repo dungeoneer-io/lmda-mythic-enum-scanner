82999b7f038982f693a82f557bf52fc82950ab9d
Chris Creighton (@creighcl) <27293513+Creighcl@users.noreply.github.com>
2022-09-21 18:47:11 -0400

    tracked prop for dungeons

--- src/bapi-mapper/mythic-enum-snapshot.js
+++ src/bapi-mapper/mythic-enum-snapshot.js
@@ -4,14 +4,16 @@ const mapApiDungeonToPeriodDungeon = ({
     map,
     zone,
     dungeon,
-    keystone_upgrades
+    keystone_upgrades,
+    is_tracked: tracked
 }) => ({
     id,
     name,
     map: map.id,
     zone: zone.slug,
     dungeon: dungeon.id,
-    upgrades: keystone_upgrades.map(({ qualifying_duration }) => qualifying_duration)
+    upgrades: keystone_upgrades.map(({ qualifying_duration }) => qualifying_duration),
+    tracked
 });
 
 const mapApiAffixToAffix = ({ id, name }) => ({ id, name });
